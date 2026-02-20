76803cbc810e566d1ec05b741f02b9886c290051
Aaron Neville <aaron@codestuff.dev>
2026-02-20 09:55:54 -0700

    removed cloudflare and added netlify support

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { cloudflare } from '@cloudflare/vite-plugin';
+import netlify from '@netlify/vite-plugin-tanstack-start';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
+    netlify(),
     tanstackStart({
       srcDirectory: 'app',
     }),
